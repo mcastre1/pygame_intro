e508bcd1a13ceda9257552f9f881edbccf68bff9
Miguel Castrejon <miguelcastrejon@mail.weber.edu>
2022-02-06 21:26:39 -0700

    Adding multiple image surfaces.

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption("Runner") # Set title of window
 clock = pygame.time.Clock()
 
-test_surface = pygame.Surface((100,200))
-test_surface.fill('red')
+# This is for making a rectangle only
+#test_surface = pygame.Surface((100,200))
+# This is for an image import
+sky_surface = pygame.image.load("sprites/Sky.png")
+ground_surface = pygame.image.load("sprites/ground.png")
+
+#test_surface.fill('red')
 
 while True:
     for event in pygame.event.get(): # Go through all events.
@@ -15,7 +20,8 @@ while True:
             pygame.quit() # Call the quit function.
             exit()
 
-    screen.blit(test_surface,(200,100))
+    screen.blit(sky_surface,(0,0))
+    screen.blit(ground_surface,(0,300))
 
     pygame.display.update()  # This updates the screen surface.
     clock.tick(60) # This tells the while loop that it shouldnt run faster than 60 frames per sec.
