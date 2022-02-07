0c239219aa49d24eb128892f6530aafbd9326e15
Miguel Castrejon <miguelcastrejon@mail.weber.edu>
2022-02-06 21:42:35 -0700

    Moving snail

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ sky_surface = pygame.image.load("sprites/Sky.png")
 ground_surface = pygame.image.load("sprites/ground.png")
 text_surface = test_font.render("My game",False,"Green") # Creating the text surface
 
+snail_surface = pygame.image.load('sprites/snail/snail1.png')
+snail_x_pos = 600
+
 #test_surface.fill('red')
 
 while True:
@@ -25,6 +28,10 @@ while True:
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
     screen.blit(text_surface,(300,50))
+    screen.blit(snail_surface,(snail_x_pos,265))
+    snail_x_pos -= 4
+    if snail_x_pos <= 0 - snail_surface.get_width():
+        snail_x_pos = 800
 
     pygame.display.update()  # This updates the screen surface.
     clock.tick(60) # This tells the while loop that it shouldnt run faster than 60 frames per sec.
