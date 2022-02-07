5698f3b7d0455a21809461c48998b34789527976
Miguel Castrejon <miguelcastrejon@mail.weber.edu>
2022-02-06 21:30:56 -0700

    Text Surface with custom font.

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption("Runner") # Set title of window
 clock = pygame.time.Clock()
+test_font = pygame.font.Font("font/Pixeltype.ttf",50) # Creating a font for displaying text on the screen.
 
 # This is for making a rectangle only
 #test_surface = pygame.Surface((100,200))
 # This is for an image import
 sky_surface = pygame.image.load("sprites/Sky.png")
 ground_surface = pygame.image.load("sprites/ground.png")
+text_surface = test_font.render("My game",False,"Green") # Creating the text surface
 
 #test_surface.fill('red')
 
@@ -22,6 +24,7 @@ while True:
 
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
+    screen.blit(text_surface,(300,50))
 
     pygame.display.update()  # This updates the screen surface.
     clock.tick(60) # This tells the while loop that it shouldnt run faster than 60 frames per sec.
